d6498ff8bb925277c4961f7a5c270cc142f1aa1b
arition <aritionkb@gmail.com>
2020-08-28 05:11:07 -0700

    build torchvision

--- src/addon.cc
+++ src/addon.cc
@@ -4,11 +4,20 @@
 #include "ScriptModule.h"
 #include "Tensor.h"
 #include "constants.h"
+#include <torchvision/vision.h>
 
 using namespace torchjs;
 
+void InitTorchVisionOps()
+{
+  // Create a useless model to ask compiler to link torchvision libs
+  auto model = vision::models::ResNet18();
+  model->eval();
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
+  InitTorchVisionOps();
   aten::Init(env, exports);
   constants::Init(env, exports);
   ScriptModule::Init(env, exports);
